de3a8e04a440c9153681c55e71808e061073967d
hp <hp@DESKTOP-SVA3L1T>
2024-11-03 23:09:27 +0530

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,53 @@
+"use client";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import Select from "./Select";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default Home = () => {
+  const router = useRouter();
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleSelectChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedValue(selectedValue); // Update state with selected value
+    router.push(selectedValue);
+  };
+
   return (
-    <>
-    </>
+    <nav className="flex justify-between mx-7 my-5 ">
+      <ul>
+        <li>Sankalp Foundation</li>
+      </ul>
+      <ul className="flex space-x-4">
+        <li>
+          <NavLink href="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink href="/about-us">About Us</NavLink>
+        </li>
+        <li>
+          <NavLink href="/donation">Donation</NavLink>
+        </li>
+        <li>
+          <Select
+            options={[
+              { value: "/environment", label: "Environment" },
+              { value: "/education", label: "Education" },
+              { value: "/social-awareness", label: "Social Awareness" },
+            ]}
+            onChange={handleSelectChange}
+            value={selectedValue} // Bind selected value to dropdown
+          />
+        </li>
+        <li>
+          <NavLink href="/news">News</NavLink>
+        </li>
+        <li>
+          <NavLink href="/contact-us">Contact Us</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
+
